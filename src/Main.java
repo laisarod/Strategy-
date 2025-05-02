cfd93550d47f3327c997f227d7c4ef10e2c69207
Laisa Rodrigues <laisarodriguesm17@gmail.com>
2025-05-02 14:24:46 -0300

    Adicionando a implementação Strategy

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        MediaStrategy estrategia = new Aritmetica();      // troque por new Geometrica()
+        MediaStrategy estrategia = new Geometrica();      // troque por new Geometrica()
         Disciplina d = new Disciplina(estrategia);
 
         d.setNome("Padrões de Desenvolvimento");
-        d.setP1(10);
+        d.setP1(8);
         d.setP2(5);
         d.calcularMedia();
 
